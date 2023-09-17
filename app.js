d29f686ddf766f43a916115562168162afd0495b
AbiodunVlad <79141726+AbiodunVlad@users.noreply.github.com>
2023-09-18 00:03:07 +0100

    Update app.js

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-//using selectors inside the element
+
 const questions = document.querySelectorAll(".question");
 
 questions.forEach(function (question) {
@@ -15,14 +15,3 @@ questions.forEach(function (question) {
   });
 });
 
-// traversing the dom
-
-// const btns = document.querySelectorAll(".question-btn");
-
-// btns.forEach(function (btn) {
-//   btn.addEventListener("click", function (e) {
-//     const question = e.currentTarget.parentElement.parentElement;
-
-//     question.classList.toggle("show-text");
-//   });
-// });
